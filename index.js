0d743766a144d3c2aff8d14a42b911f6353a1d80
Cyber Falcon <thiolimor@gmail.com>
2026-03-30 15:31:36 -0300

    feat(script): Add the operate function

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
+let numberInMemory = 0;
+let numberInScreen = 0;
+let operator = null;
+
+// numberInMemory = 3;
+// numberInScreen = 4;
+// operator = sum;
+// operate();
+
+function operate() {
+  const result = operator(numberInMemory, numberInScreen);
+  console.log("result = ", result);
+  numberInMemory = result;
+}
+
+function operateWith(a, b, func) {
+  return func(a, b);
+}
+
 function sum(a, b) {
   return a + b;
 }
