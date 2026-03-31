76e8879f0ff37662542ad9681fe074ad2d375a0e
Cyber Falcon <thiolimor@gmail.com>
2026-03-30 21:16:18 -0300

    feat: Add chained operations

--- index.js
+++ index.js
@@ -1,17 +1,87 @@
 class Calculator {
-  numberInMemory = null;
+  cachedResult = null;
   numberInScreen = null;
   operator = null;
 
+  pressNumber(number) {
+    this.numberInScreen = number;
+  }
 
+  pressSum() {
+    this.operator = this.#sum;
+    this.#afterOperatorPressed();
+  }
+  pressSubtract() {
+    this.operator = this.#subtract;
+    this.#afterOperatorPressed();
+  }
+  pressMultiply() {
+    this.operator = this.#multiply;
+    this.#afterOperatorPressed();
+  }
+  pressDivide() {
+    this.operator = this.#divide;
+    this.#afterOperatorPressed();
+  }
+
+  #afterOperatorPressed() {
+    const numberWasPressed = this.numberInScreen !== null;
+    const hasCachedResult = this.cachedResult !== null;
+    if (isFirstButtonPress()) {
+      this.cachedResult = 0;
+      return;
+    }
+
+    if (isFirstCalculation()) {
+      this.cachedResult = this.numberInScreen;
+      this.numberInScreen = null;
+      return;
+    }
+
+    if (isChainOperation()) {
+      this.#calculate();
+      return;
+    }
+
+    if (isOverridingOperation()) {
+      return;
+    }
+
+    /*
+     * Has no Cache + User hasn't Pressed  (First button, without number)
+     *    Use `0` as Cache
+     * Has no Cache + User Pressed (First calculation)
+     *    Use the Pressed number as Cache
+     * Has Cache + User Pressed (Valid chain calculation)
+     *    Calculate
+     * Has Cache + User hasn't Pressed (Just made a calculation)
+     *    Invalid Operation (?)
+     */
+
+    function isFirstButtonPress() {
+      return !hasCachedResult && !numberWasPressed;
+    }
+    function isFirstCalculation() {
+      return !hasCachedResult && numberWasPressed;
+    }
+    function isChainOperation() {
+      return hasCachedResult && numberWasPressed;
+    }
+    function isOverridingOperation() {
+      return hasCachedResult && !numberWasPressed;
+    }
+  }
 
   #calculate() {
     const result = this.#operate(
-      this.numberInMemory,
+      this.cachedResult,
       this.numberInScreen,
       this.operator,
     );
     console.log("result = ", result);
+    this.cachedResult = result;
+    this.numberInScreen = null;
+    this.operator = null;
   }
 
   #operate(a, b, func) {
@@ -35,9 +105,29 @@ class Calculator {
   }
 }
 
-const calculator = new Calculator();
+function runTests() {
+  let calculator = new Calculator();
+  calculator.pressSum();
+  console.log("cached should be `0`", calculator);
 
-calculator.numberInMemory = 3;
-calculator.numberInScreen = 4;
-// calculator.operator = calculator.#sum;
-// calculator.#calculate();
+  calculator = new Calculator();
+  calculator.pressNumber(3);
+  calculator.pressSum();
+  console.log("cached should be `3` and screen should be `null`", calculator);
+
+  calculator = new Calculator();
+  calculator.pressNumber(3);
+  calculator.pressSum();
+  calculator.pressNumber(4);
+  calculator.pressSum();
+  console.log("cached should be `7` and screen should be `null`", calculator);
+
+  calculator = new Calculator();
+  calculator.pressNumber(3);
+  calculator.pressSum();
+  calculator.pressSubtract();
+  console.log(
+    "cached should be `3` and operator should be `subtract`",
+    calculator,
+  );
+}
--- readme.md
+++ readme.md
@@ -17,7 +17,7 @@ We plan to separate the project into distinct frontend and backend so that we ca
   - Don’t worry about making them functional just yet.
   - There should also be a display for the calculator. Go ahead and fill it with some dummy numbers so it looks correct.
   - Add a “clear” button.
-- [ ] Create the functions that update one of your number variables when the calculator’s digit buttons are clicked. Your calculator’s display should also update to reflect the value of that number variable.
+- [/] Create the functions that update one of your number variables when the calculator’s digit buttons are clicked. Your calculator’s display should also update to reflect the value of that number variable.
 - [ ] Make the calculator work! You’ll need to store the first and second numbers input by the user and then operate() on them when the user presses the = button, according to the operator that was selected between the numbers.
   - You should already have the code that can populate the display, so once operate has been called, update the display with the result of the operation.
   - This is the hardest part of the project. You need to figure out how to store all the values and call the operate function with them. Don’t feel bad if it takes you a while to figure out the logic.
